af1894a4057d4f5b47bb76c9d8d82d0b2c7f0896
Callum Hopkins <103919468@student.swin.edu.au>
2022-05-10 08:43:58 +1000

    Created file

---
+++ delegates-and-lambda-tasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace delegates_and_lambda_tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
